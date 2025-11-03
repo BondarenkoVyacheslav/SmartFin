70fb3cc1f75c0c8f43a37b83b7a580d38709d2ba
Sepl-flek <154527662+Sepl-flek@users.noreply.github.com>
2025-11-03 16:02:14 +0300

    create authorization

--- SmartFin/app/account/views.py
+++ SmartFin/app/account/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"email": request.user.email, "username": request.user.username})
