9906b206f49d2735ffb9af37644be76f5950d7f3
VerneR100 <slavaboond2000@gmail.com>
2025-12-20 22:49:12 +0300

    feat: сделал таблицу в бд для снимков стоимости кажого типа пользовательского актива.

--- backend/app/analytics/models.py
+++ backend/app/analytics/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from app.assets.models import AssetType
 from app.portfolio.models import Portfolio
 
 
@@ -29,3 +30,29 @@ class PortfolioDailySnapshot(models.Model):
 
     def __str__(self) -> str:
         return f"{self.portfolio_id} {self.snapshot_date}"
+
+
+
+class PortfolioAssetDailySnapshot(models.Model):
+    portfolio = models.ForeignKey(
+        Portfolio,
+        on_delete=models.CASCADE,
+        related_name="asset_type_daily_snapshot",
+    )
+    asset_type = models.ForeignKey(
+        AssetType,
+        on_delete=models.CASCADE,
+        related_name="portfolio_daily_snapshots_by_type",
+    )
+    snapshot_date = models.DateField()
+    snapshot = models.DecimalField(max_digits=20, decimal_places=8)
+    margin = models.DecimalField(max_digits=20, decimal_places=8)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["portfolio", "asset_type", "snapshot_date"],
+                name="uniq_portfolio_asset_type_day",
+            )
+        ]
+        ordering = ["-snapshot_date", "-id"]
--- backend/app/integrations/mutations.py
+++ backend/app/integrations/mutations.py
@@ -5,6 +5,6 @@ from .queries import IntegrationType
 @strawberry.type
 class IntegrationMutations:
     @strawberry.mutation
-    def create_integration(self, portfolio_id: int, exchange_id: int, key: str) -> IntgrationType:
+    def create_integration(self, portfolio_id: int, exchange_id: int, key: str) -> IntegrationType:
         return IntegrationType.objects.create(portfolio_id= portfolio_id, exchange_id=exchange_id, key=key)
 
