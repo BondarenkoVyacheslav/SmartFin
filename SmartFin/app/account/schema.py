f48c22771b939e74dfe39d52ad727b19f39866e9
Sepl-flek <154527662+Sepl-flek@users.noreply.github.com>
2026-01-21 13:29:00 +0300

    add graphql to user mutations and queries

---
+++ SmartFin/app/account/schema.py
@@ -0,0 +1,7 @@
+import strawberry
+from .mutations import UserMutations
+from .queries import UserQueries
+
+
+UserQuery = UserQueries
+UserMutation = UserMutations
--- SmartFin/config/shcema.py
+++ SmartFin/config/shcema.py
@@ -2,15 +2,16 @@ import strawberry
 from app.assets.schema import AssetQuery, AssetMutation
 from app.portfolio.schema import PortfolioQuery, PortfolioMutation
 from app.transaction.schema import TransactionQuery, TransactionMutation
+from app.account.schema import UserQuery, UserMutation
 
 
 @strawberry.type
-class Query(AssetQuery, PortfolioQuery, TransactionQuery):
+class Query(AssetQuery, PortfolioQuery, TransactionQuery, UserQuery):
     pass
 
 
 @strawberry.type
-class Mutation(AssetMutation, PortfolioMutation, TransactionMutation):
+class Mutation(AssetMutation, PortfolioMutation, TransactionMutation, UserMutation):
     pass
 
 
