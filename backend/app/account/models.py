01192fc5601fd06a519846beff990284422e1f4a
vyacheslavik <slavaboond2000@gmail.com>
2026-01-27 21:07:19 +0300

    fix: вернул старую аторизацию app-account, убрал не ннужные миграции. Вырезал не нужные query and mutations from schema.py

--- backend/app/account/models.py
+++ backend/app/account/models.py
@@ -6,13 +6,9 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    has_subscription = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-
-    class Meta:
-        db_table = '"account"."user"'
 
     def __str__(self):
         return self.email
